2e470676b15b904e7d85a473bb05ca4beab159a8
jinpeng <jinpeng.dong@gmail.com>
2014-07-02 20:49:58 +0800

    initial check in.

---
+++ jitu-server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '<Access Key>',
+    'SECRET_KEY': '<Secret Key>',
+    'Bucket_Name': 'jitu',
+    'Port': 18080,
+    'Uptoken_Url': '/uptoken',
+    'Domain': 'http://jitu.qiniudn.com/'
+};
